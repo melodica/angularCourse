32da0a60568f91d4485d2037bcaa08abd3db921c
melodica <melodica@github.com>
2016-10-31 01:16:49 -0400

    assignment1

---
+++ module1/app.js
@@ -0,0 +1,27 @@
+(function(){
+  angular
+    .module('myApp', [])
+    .controller('lunchCheckController', myController);
+
+    lunchCheckController.$inject = ['$scope'];
+    function myController($scope){
+      $scope.message = "";
+      $scope.lunch = "";
+      $scope.items = [];
+
+      $scope.checklunch = function(){
+         $scope.items = $scope.lunch.split(/\s*,\s*/).filter(item => item);
+         var num = $scope.items.length;
+         if( num == 0){
+           $scope.message = "Please enter valid data first!";
+           $scope.checkResult = "has-error";
+         }else if( num > 0 && num <= 3){
+           $scope.message = "Enjoy!";
+           $scope.checkResult = "has-success";
+         }else if( num > 3){
+           $scope.message = "Too much!";
+           $scope.checkResult = "has-success";
+         }
+       }
+    }
+})();
