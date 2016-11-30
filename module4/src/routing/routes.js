1e50065cb30d56943f6742f1b14f35a3972b37d2
melodica <melodica@github.com>
2016-11-29 21:37:36 -0500

    route.js templateUrl error

--- module4/src/routing/routes.js
+++ module4/src/routing/routes.js
@@ -11,13 +11,13 @@
         // Home page
         .state('home', {
           url: '/',
-          templateUrl: '/src/app/app.home.html'
+          templateUrl: 'src/app/app.home.html'
         })
 
         // Premade list page
         .state('categories', {
           url: '/categories',
-          templateUrl: '/src/app/app.category.html',
+          templateUrl: 'src/app/app.category.html',
           controller: 'CategoriesController as categoriesList',
           resolve: {
             categories: ['menuDataService', function (menuDataService) {
@@ -30,7 +30,7 @@
 
         .state('items', {
           url: '/items/{categoryName}',
-          templateUrl: '/src/app/app.item.html',
+          templateUrl: 'src/app/app.item.html',
           controller: 'MenuItemController as itemList',
           resolve: {
             items : ['$stateParams', 'menuDataService',
